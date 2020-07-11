5a07374790efa886e79808314116f18632ee5c13
@raspiduino <68118236+raspiduino@users.noreply.github.com>
2020-07-11 15:41:26 +0700

    Update
    Reduce the use of RAM

--- shell.ino
+++ shell.ino
@@ -1,5 +1,5 @@
 //UNIX-like bash shell for Arduino
-//Version 1.0.0
+//Version 1.0.2
 //@raspiduino 2020
 //Date Created 7/7/2020
 /****************************************************************************************************************************************************************/
@@ -10,20 +10,19 @@ String command = "";
 
 void setup() { //Run once
   Serial.begin(9600); //Open serial port
-  Serial.println("Welcome to the UNIX-like bash shell for Arduino!");
-  Serial.print("Version ");
+  Serial.println(F("Welcome to the UNIX-like bash shell for Arduino!"));
+  Serial.print(F("Version "));
   Serial.println(version_str);
-  Serial.println("@raspiduino 2020");
+  Serial.println(F("@raspiduino 2020"));
   //Serial.println("Please type to the console and you MUST type enter when you finish, DO NOT press the send button!"); //We don't need that anymore :)))))
-  Serial.print("/> ");
+  Serial.print(F("/> "));
 }
 
 void loop() { //Run repeatedly
   String command = input();
-  Serial.println(command);
   end_command = 0; //Set the end_command back to 0
   input_str = "";  //Set the command back to "" (empty)
   exec(command);
   Serial.print(directory);
-  Serial.print("> ");
+  Serial.print(F("> "));
 }
